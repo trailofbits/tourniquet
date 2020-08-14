00eefe9b75ec3e84b00eb88637cee0623e69c1b0
Carson Harmon <carson.harmon@trailofbits.com>
2020-08-13 22:32:59 -0400

    Set up basic sql schema and automatically store AST function

--- tourniquet/tourniquet.py
+++ tourniquet/tourniquet.py
@@ -4,44 +4,29 @@ import extractor
 import sqlite3
 from sqlite3 import Error
 import logging
+import json
 
 
 class Tourniquet:
     SQL_CREATE_MODULES_TABLE = """ 
-        CREATE TABLE IF NOT EXISTS {} (
-            func_id int NOT NULL AUTO_INCREMENT,
-            function_name nvarchar NOT NULL,
-            function_table_id int NOT NULL 
-        PRIMARY KEY (func_id)
+        CREATE TABLE IF NOT EXISTS '{}' (
+            id INTEGER PRIMARY KEY,
+            function_name nvarchar NOT NULL
         ); 
     """
-    SQL_CREATE_FUNCTION_TABLE = """
-        CREATE TABLE IF NOT EXISTS {} (
-            function_table_id int NOT NULL AUTO_INCREMENT,
-            function_name nvarchar NOT NULL, 
-            var_table int, 
-            stmt_table int,
-            global_vars_table int,
-        PRIMARY KEY(function_table_id) 
+    SQL_INSERT_MOD_TABLE = """
+        INSERT INTO '{}' (function_name) VALUES ('{}')
+    """
+
+    SQL_CREATE_FUNC_TABLE = """
+            CREATE TABLE IF NOT EXISTS '{}' (
+            id INTEGER PRIMARY KEY, 
+            entry_type nvarchar NOT NULL,
+            data json NOT NULL
         );
     """
-    SQL_CREATE_VAR_TABLE = """
-        CREATE TABLE IF NOT EXISTS {} (
-            id int NOT NULL AUTO_INCREMENT, 
-            name nvarchar NOT NULL, 
-            var_type int NOT NULL, 
-            is_array int NOT NULL, 
-            var_size int NOT NULL,
-        PRIMARY_KEY(id) 
-        );
-    """
-    SQL_CREATE_STMT_TABLE = """
-        CREATE TABLE IF NOT EXISTS {} (
-            id int NOT NULL AUTO_INCREMENT, 
-            stmt_type int NOT NULL, 
-            json_blob json NOT NULL,
-        PRIMARY_KEY(id)
-        );
+    SQL_INSERT_FUNC_ENTRY = """
+        INSERT INTO '{}' (entry_type, data) VALUES ( '{}', '{}' )
     """
 
     def __init__(self, database_name: str):
@@ -76,17 +61,48 @@ class Tourniquet:
                 clean_result[decoded_key].append(list(map(lambda x: x.decode("utf-8"), entry)))
         return clean_result
 
-    def create_var_table(self, table_name: str, table_entries: List[str]) -> None:
+    def create_module_table(self, table_name: str) -> int:
         cursor = self.db_conn.cursor()
-        # Create new table
-        cursor.execute(self.SQL_CREATE_VAR_TABLE.format(table_name))
+        cursor.execute(self.SQL_CREATE_MODULES_TABLE.format(table_name))
+        self.db_conn.commit()
+        return cursor.lastrowid
+
+    # The way to relate to this table is via a string
+    def create_global_table(self, module_name) -> str:
+        cursor = self.db_conn.cursor()
+        cursor.execute(self.SQL_CREATE_FUNC_TABLE.format(module_name + "_globals"))
+        self.db_conn.commit()
+        return module_name + "_globals"
+
+    def create_function_table(self, table_name: str, table_entries) -> None:
+        cursor = self.db_conn.cursor()
+        table_create_query = self.SQL_CREATE_FUNC_TABLE.format(table_name)
+        print(table_create_query)
+        cursor.execute(table_create_query)
+        for entry in table_entries:
+            print("ENTRY IS ", entry)
+            print("TABLE ENTRY IS", table_entries)
+            entry_type = entry[len(entry) - 1]
+            print(entry_type)
+            query = self.SQL_INSERT_FUNC_ENTRY.format(table_name, entry_type, json.dumps(entry))
+            print(query)
+            cursor.execute(query)
+        self.db_conn.commit()
 
     def store_ast(self, ast_info: Dict[str, List[List[str]]]) -> None:
+        # Create module table
+        module_name = "test_name"
+        self.create_module_table(module_name)
+        # create global table
+        global_table = self.create_global_table(module_name)
         # Table for each function (maybe some symbol issues)
+        cursor = self.db_conn.cursor()
         for func_key in ast_info:
             if func_key == "global":
-
-
-        # Inside each function table there are all the variables
-        # and references to stmt tables
+                continue
+            self.create_function_table(func_key, ast_info[func_key])
+            mod_insert_query = self.SQL_INSERT_MOD_TABLE.format(module_name, func_key)
+            print(mod_insert_query)
+            cursor.execute(mod_insert_query)
+            self.db_conn.commit()
         return
