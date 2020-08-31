23ceb6c52dafa84b04316fac52525eb691edf2a6
William Woodruff <william@trailofbits.com>
2020-08-31 16:48:08 -0400

    tourniquet/models: isort fixes

--- tourniquet/models.py
+++ tourniquet/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
 
 Base = declarative_base()
 
