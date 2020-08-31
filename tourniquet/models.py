93463b99aa1a5f239df8b9e4ee401c59a8ef3191
William Woodruff <william@trailofbits.com>
2020-08-31 16:44:41 -0400

    tourniquet/models: Fill in relationships

--- tourniquet/models.py
+++ tourniquet/models.py
@@ -1,5 +1,6 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Boolean, Column, Integer, String
 
 Base = declarative_base()
 
@@ -10,33 +11,50 @@ class Module(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
-    # line_map = ...
-    # functions = ...
-    # globals = ...
+    functions = relationship("Function")
+    global_variables = relationship("Global")
 
 
 class Function(Base):
     __tablename__ = "functions"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    module_id = Column(Integer, ForeignKey("modules.id"))
+    module = relationship("Module", back_populates="functions")
 
+    name = Column(String)
     start_line = Column(Integer)
     start_column = Column(Integer)
 
-    # var_decls = ...
-    # calls = ...
-    # statements = ...
+    var_decls = relationship("VarDecl")
+    calls = relationship("Call")
+    statements = relationship("Statement")
 
 
 class Global(Base):
     __tablename__ = "globals"
+
+    id = Column(Integer, primary_key=True)
+    module_id = Column(Integer, ForeignKey("modules.id"))
+    module = relationship("Module", back_populates="functions")
+
+    name = Column(String)
+    type_ = Column(String)
+    start_line = Column(Integer)
+    start_column = Column(Integer)
+    end_line = Column(Integer)
+    end_column = Column(Integer)
+    is_array = Column(Boolean)
+    size = Column(Integer)
 
 
 class VarDecl(Base):
     __tablename__ = "var_decls"
 
     id = Column(Integer, primary_key=True)
+    function_id = Column(Integer, ForeignKey("functions.id"))
+    function = relationship("Function", back_populates="var_decls")
+
     name = Column(String)
     type_ = Column(String)
     start_line = Column(Integer)
@@ -51,22 +69,32 @@ class Call(Base):
     __tablename__ = "calls"
 
     id = Column(Integer, primary_key=True)
+    function_id = Column(Integer, ForeignKey("functions.id"))
+    function = relationship("Function", back_populates="calls")
+
     expr = Column(String)
     name = Column(String)
 
-    # arguments = ...
+    arguments = relationship("Argument")
 
 
 class Argument(Base):
     __tablename__ = "call_arguments"
 
     id = Column(Integer, primary_key=True)
+    call_id = Column(Integer, ForeignKey("calls.id"))
+    call = relationship("Call", back_populates="arguments")
+
     name = Column(String)
     type_ = Column(String)
 
 
 class Statement(Base):
     __tablename__ = "statements"
+
+    id = Column(Integer, primary_key=True)
+    function_id = Column(Integer, ForeignKey("functions.id"))
+    function = relationship("Function", back_populates="statements")
 
     start_line = Column(Integer)
     start_column = Column(Integer)
