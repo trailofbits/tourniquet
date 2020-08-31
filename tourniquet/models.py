222989c1848c6ed7b3f16d3097870a36ad55d56a
William Woodruff <william@trailofbits.com>
2020-08-31 17:01:04 -0400

    tourniquet/models: Make flake8 happy

--- tourniquet/models.py
+++ tourniquet/models.py
@@ -2,7 +2,6 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-
 Base = declarative_base()
 
 
