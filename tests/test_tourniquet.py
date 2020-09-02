975166db1793e6c3083e896d9e1518dd02e4007c
William Woodruff <william@trailofbits.com>
2020-09-02 17:43:50 -0400

    tests: Begin filling in DB tests

--- tests/test_tourniquet.py
+++ tests/test_tourniquet.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pytest
 
 from tourniquet import Tourniquet
+from tourniquet.models import Function, Global, Module
 from tourniquet.patch_lang import FixPattern, NodeStmt, PatchTemplate
 
 TEST_DIR = Path(__file__).resolve().parent
@@ -37,6 +38,35 @@ def test_tourniquet_extract_ast(tmp_db):
     # There are 4 variables in "main", plus "argc" and "argv".
     main_vars = [var_decl[5] for var_decl in main if var_decl[0] == "var_type"]
     assert set(main_vars) == {"argc", "argv", "buff", "buff_len", "pov", "len"}
+
+
+def test_tourniquet_db(tmp_db):
+    tourniquet = Tourniquet(tmp_db)
+    test_file = TEST_FILE_DIR / "patch_test.c"
+    tourniquet.collect_info(test_file)
+
+    module = tourniquet.db.query(Module).filter_by(name=str(test_file)).one()
+    assert len(module.functions) == 1
+
+    pass_ = tourniquet.db.query(Global).filter_by(name="pass").one()
+    assert pass_.name == "pass"
+    assert pass_.type_ == "char *"
+    assert pass_.start_line == 20
+    assert pass_.start_column == 1
+    assert pass_.end_line == 20
+    assert pass_.end_column == 23
+    assert not pass_.is_array
+    assert pass_.size == 8
+
+    main = tourniquet.db.query(Function).filter_by(name="main").one()
+    assert main.name == "main"
+    assert main.start_line == 22
+    assert main.start_column == 1
+    assert main.end_line == 38
+    assert main.end_column == 1
+    assert len(main.var_decls) == 6
+    assert len(main.calls) == 4
+    assert len(main.statements) == 10
 
 
 def test_tourniquet_extract_ast_invalid_file(tmp_db):
