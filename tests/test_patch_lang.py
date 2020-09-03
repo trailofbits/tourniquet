8b50c935edc049115322e69f894c75c492336ce3
William Woodruff <william@trailofbits.com>
2020-09-03 14:08:05 -0400

    test_patch_lang: More tests

--- tests/test_patch_lang.py
+++ tests/test_patch_lang.py
@@ -1,5 +1,22 @@
 from tourniquet import Tourniquet
-from tourniquet.patch_lang import FixPattern, NodeStmt, PatchTemplate, Variable
+from tourniquet.patch_lang import (
+    BinaryBoolOperator,
+    BinaryMathOperator,
+    ElseStmt,
+    FixPattern,
+    IfStmt,
+    LessThanExpr,
+    Lit,
+    NodeStmt,
+    PatchTemplate,
+    StaticBufferSize,
+    Variable
+)
+
+
+def test_concretize_lit():
+    lit = Lit("1")
+    assert set(lit.concretize(None, None, None, None)) == {"1"}
 
 
 def test_concretize_variable(test_files, tmp_db):
@@ -14,11 +31,31 @@ def test_concretize_variable(test_files, tmp_db):
 
 
 def test_concretize_staticbuffersize(test_files, tmp_db):
-    pass
+    tourniquet = Tourniquet(tmp_db)
+    test_file = test_files / "patch_test.c"
+    tourniquet.collect_info(test_file)
+
+    sbs = StaticBufferSize()
+    concretized = set(sbs.concretize(23, 3, tourniquet.db, str(test_file)))
+    assert concretized == {"sizeof(buff)"}
 
 
-def test_concretize_binarymathoperator(test_files, tmp_db):
-    pass
+def test_concretize_binarymathoperator():
+    bmo = BinaryMathOperator(Lit("1"), Lit("2"))
+    concretized = set(bmo.concretize(None, None, None, None))
+    assert concretized == {"1 + 2", "1 - 2", "1 / 2", "1 * 2", "1 << 2"}
+
+
+def test_concretize_binarybooloperator():
+    bbo = BinaryBoolOperator(Lit("1"), Lit("2"))
+    concretized = set(bbo.concretize(None, None, None, None))
+    assert concretized == {"1 == 2", "1 != 2", "1 <= 2", "1 < 2", "1 >= 2", "1 > 2"}
+
+
+def test_concretize_lessthanexpr():
+    lte = LessThanExpr(Lit("1"), Lit("2"))
+    concretized = set(lte.concretize(None, None, None, None))
+    assert concretized == {"1 < 2"}
 
 
 def test_concretize_nodestmt(test_files, tmp_db):
@@ -30,6 +67,31 @@ def test_concretize_nodestmt(test_files, tmp_db):
     concretized = list(node.concretize(23, 3, tourniquet.db, str(test_file)))
     assert len(concretized) == 1
     assert concretized[0] == "char buff[10];;"
+
+
+# TODO(ww): Tests for:
+# * Statement
+# * StatementList
+# * IfStmt
+# * ElseStmt
+# * ReturnStmt
+
+
+def test_fixpattern():
+    fp = FixPattern(IfStmt(Lit("1"), Lit("exit(1);")), ElseStmt(Lit("exit(2);")))
+    concretized = list(fp.concretize(None, None, None, None))
+    assert len(concretized) == 1
+    assert (
+        concretized[0]
+        == """if (1) {
+exit(1);
+}
+
+else {
+exit(2);
+}
+"""
+    )
 
 
 def test_patch():
